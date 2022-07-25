08d9e87e2d86969ffb5f1c4c82301d687e727a50
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-07-25 18:47:13 +0200

    fix: created and updated at fields

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -10,6 +11,9 @@ class User(AbstractUser):
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.username
+
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -17,12 +21,16 @@ class Lead(models.Model):
     age = models.IntegerField(default=0)
     agent = models.ForeignKey(Agent, on_delete=models.SET_NULL, null=True)
 
-    created_at = models.DateTimeField(null=True)
-    updated_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(default=datetime.now)
+    updated_at = models.DateTimeField(default=datetime.now)
 
     def save(self, *args, **kwargs):
         ''' On save, update timestamps '''
-        if not self.id:
-            self.created_at = datetime.now()
         self.updated_at = datetime.now()
         return super(Lead, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('lead:detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
