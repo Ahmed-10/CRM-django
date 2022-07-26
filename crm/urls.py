e052abc1818c4e78b029df43b3e2fc5cfdbd70ff
ahmed-10 <ahmedalaa.10@hotmail.com>
2022-07-26 21:30:13 +0200

    feat: add login and logout routes

--- crm/urls.py
+++ crm/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from .views import LandingPageView
 
@@ -23,6 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing_page'),
     path('leads/', include('leads.urls', namespace='lead')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
